8cd8c3ad9185b02225735f0066a1f799173c9312
Salvatore Novelli <salvatore.novelli@gmail.com>
2019-07-21 09:37:36 +0100

    init

---
+++ src/main/java/com/github/salvatorenovelli/crawlercommons/CrawlerCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.github.salvatorenovelli.crawlercommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerCommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/salvatorenovelli/crawlercommons/CrawlerCommonsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.salvatorenovelli.crawlercommons;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrawlerCommonsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
